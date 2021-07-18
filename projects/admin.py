81dafd052ddc94cb871bb90bbd7d628f81350959
annechege <annechege780@gmail.com>
2021-07-19 02:51:44 +0300

    in admin importing profile,projects review

--- projects/admin.py
+++ projects/admin.py
@@ -1,9 +1,11 @@
+# just the owner can be able to visit the site and edit 
+
 from django.contrib import admin
-from .models import Profile,Projects,Revieww
+from .models import Profile,Projects,Review
 from django.contrib import admin
 
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Projects)
-admin.site.register(Revieww)
+admin.site.register(Review)
